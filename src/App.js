fb9147996201d94b2742f4744cd70757c8972ab6
Anastasia <anastasia.diudina@gmail.com>
2018-08-29 16:48:31 +0300

    Create new react app with bootstrap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p className="btn btn-primary">
+          This is how everything starts
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
